083f47b761eddccaa9f59fddcf135b055354f389
Neeka0909 <r.nisalvimukthi@gmail.com>
2022-11-03 23:57:01 +0530

    Complete Development.

--- Roll_a_Dice/Program.cs
+++ Roll_a_Dice/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-
+using System.Security.Cryptography.X509Certificates;
 
 public class gamePlayer
 {
     public int id;
     public String name;
-    public int turn;
     public int score;
     
     public gamePlayer(int id, String name)
@@ -14,13 +13,6 @@ public class gamePlayer
         this.name = name;
     }
 
-    public gamePlayer(int id, string name, int turn, int score)
-    {
-        this.id = id;
-        this.name = name;
-        this.turn = turn;
-        this.score = score;
-    }
 
     public int getId()
     {
@@ -32,10 +24,6 @@ public class gamePlayer
         return name;
     }
 
-    public int getTurn()
-    {
-        return turn;
-    }
 
     public int getScore()
     {
@@ -52,11 +40,6 @@ public class gamePlayer
         name = newName;
     }
 
-    public void setTurn(int newTurn)
-    {
-        turn = newTurn;
-    }
-
     public void setScore(int newScore)
     {
         score = newScore;
@@ -66,6 +49,16 @@ public class gamePlayer
     {
         void programStart()
         {
+            String title = @"
+                    .______        ______    __       __              ___          _______   __    ______  _______ 
+                    |   _  \      /  __  \  |  |     |  |            /   \        |       \ |  |  /      ||   ____|
+                    |  |_)  |    |  |  |  | |  |     |  |           /  ^  \       |  .--.  ||  | |  ,----'|  |__   
+                    |      /     |  |  |  | |  |     |  |          /  /_\  \      |  |  |  ||  | |  |     |   __|  
+                    |  |\  \----.|  `--'  | |  `----.|  `----.    /  _____  \     |  '--'  ||  | |  `----.|  |____ 
+                    | _| `._____| \______/  |_______||_______|   /__/     \__\    |_______/ |__|  \______||_______|
+                                                                                               
+                     ";
+            Console.WriteLine(title);
             Console.WriteLine("Welcome to Roll A Dice Game...");
             Console.WriteLine("Please choose from one of the following options...");
             Console.WriteLine("1. Start the Game.");
@@ -75,12 +68,13 @@ public class gamePlayer
 
         void showRules()
         {
+            Console.WriteLine("Game Rules \n");
             Console.WriteLine("* Number of Players: 02  \n* Number of Players: 02 \n* Winning Score: 100 \n");
             Console.WriteLine("* Each player will get a chance to roll two dices at the same time. The process given below will \r\ncontinues until one player reaches the winning score. \n* If 02 dices give the same result (except the dice value 1), the same player will get another \r\nchance to roll the dice.\n* Else other player gets the chance to roll 02 dices.");
             Console.WriteLine("\n \nCalculation of score: \n* If both dices give the value 1, the individual marks of the respective player will be \r\nreduced to 0.\r\n* Else sum of the two dice values will be added to individual scores of the respective \r\nplayer.\r\n");
             Console.WriteLine("Please choose from one of the following options...");
-            Console.WriteLine("1. Back to Start window.");
-            Console.WriteLine("2. Exit the Game");
+            Console.WriteLine("1. Start The Game");
+            Console.WriteLine("2. Exit to main menu.");
 
             int optionRules = 0;
 
@@ -90,9 +84,13 @@ public class gamePlayer
                 {
                     optionRules = int.Parse(Console.ReadLine());
                 }
-                catch { Console.WriteLine("Please Enter the correct value."); }
-                if (optionRules == 1) { programStart(); }
-                else if (optionRules == 2) { break; }
+                catch 
+                { 
+                    Console.WriteLine("Please Enter the correct value.");
+                    showRules();
+                }
+                if (optionRules == 1) { gamePlay();}
+                else if (optionRules == 2) { break;}
             }while(optionRules != 2);
             {
                 Console.WriteLine("Thank You, have a Nice Day!!");
@@ -104,6 +102,7 @@ public class gamePlayer
 
         //Player Details saved
         void playerAdd()
+
         {
             String playerName = "";
             bool count = true;
@@ -112,69 +111,163 @@ public class gamePlayer
             {
                 Console.WriteLine("Please enter Player " + i + " Name :");
                 playerName = Console.ReadLine();
-                player.Add(new gamePlayer(i, playerName, 0 , 0));
+                player.Add(new gamePlayer(i, playerName));
                 i++;   
                 if (i == 3)
                 {
                     count=false;
                 }
             }
-            
-
+            Console.WriteLine("Press any key to Continue and Play...");
+            Console.ReadKey();
         }
-        //Player selection do not need player 1 start the game
+
+        //Player score count function
         gamePlayer currentPlayer;
-        void playerSelect()
+
+        void scoreCount(int j)
         {
-            Console.WriteLine("Please Select Player :");
-            int j;
-            for (int i = 0; i < player.Count; i++)
+            int x = 0;
+            int y = 0;
+            int playerScore = 0;
+
+            currentPlayer = player[j];
+            Console.WriteLine(currentPlayer.getName() + " Press Any key to Roll a dice.");
+            Console.ReadKey();
+
+            x = rollADice().Item1;
+            y = rollADice().Item2;
+
+            Console.WriteLine("\nDice 1 value : " + x);
+            Console.WriteLine("Dice 2 value : " + y);
+
+            if (x == 1 && y == 1)
             {
-                currentPlayer = player[i];
-                j = i + 1;
-                Console.WriteLine(j + ". "+ currentPlayer.getName() +".");
+                currentPlayer.setScore(0);
+                Console.WriteLine(currentPlayer.getName() + " Occures tie Event. Both Dices give same value. \r\n Then Set " + currentPlayer.getName() + " Score to 0.");
             }
-            int playerOption = 0;
-
-            do
+            else if (x == y)
             {
-                try
-                {
-                    playerOption = int.Parse(Console.ReadLine());
-                }
-                catch { Console.WriteLine("Incorrect input !!!"); }
-
-                if (playerOption==1)
-                {
-                    currentPlayer = player[1];
-                    Console.WriteLine("Press any Key to Roll a Dice...");
-                    Console.ReadKey();
-                    rollADice(currentPlayer);
-
-                }
-
-            }while (playerOption != 0);
+                Console.WriteLine("Both Dices gives the Same Value.");
+                scoreCount(j);
+            }
+            else
+            {
+                playerScore = x + y;
+                currentPlayer.setScore(playerScore + currentPlayer.getScore());
+                Console.WriteLine("\nTotal Score of " + currentPlayer.getName() + " = " + currentPlayer.getScore());
+            }
 
         }
 
-        var x = 0;
-        var y = 0;
+
         //Dice Role local function
-        void rollADice(gamePlayer currentPlayer)
+        Tuple<int, int> rollADice()
         {
             Random rand = new Random();
-            x = rand.Next(0, 7);
-            y = rand.Next(0, 7);
+            var x = rand.Next(1, 7);
+            var y = rand.Next(1, 7);
 
-            Console.WriteLine("\nDice 1 Result: " + x);
-            Console.WriteLine("Dice 1 Result: " + y + "\n");
-
-            if (x == 1 && y ==1)
-            {
-
-            }
-
+            return Tuple.Create(x, y);
         }
 
+
+
+        void gamePlay(){
+
+            playerAdd();
+            gamePlayer Player1 = player[0];
+            gamePlayer Player2 = player[1];
+            int i = 0;
+
+            while (true)
+            {
+
+                if (Player1.getScore() > 100 || Player2.getScore() > 100)
+                {
+                   
+                    if (Player1.getScore() > 100)
+                    {
+                        Console.WriteLine("The Winner is : " + Player1.getName());
+                    }
+                    else if (Player2.getScore() > 100)
+                    {
+                        Console.WriteLine("The Winner is : " + Player2.getName());
+                    }
+                    exit();
+                    Player1.setScore(0);
+                    Player2.setScore(0);
+                    Console.WriteLine("\nPress any key to Start again...");
+                    Console.ReadKey();
+                    programRun();
+                    break;
+                }
+                else
+                {
+                    scoreCount(i);
+                    i++;
+
+                }
+
+                if (i == 2)
+                {
+                    //give 2nd player start
+                    i = i - 2;
+                }
+            }
+        }
+
+        //game exit
+        void exit()
+        {
+            gamePlayer gamePlayer1 = player[0];
+            gamePlayer gamePlayer2 = player[1];
+            Console.WriteLine(gamePlayer1.getName() + " Score is : " + gamePlayer1.getScore());
+            Console.WriteLine(gamePlayer2.getName() + " Score is : " + gamePlayer2.getScore());
+        }
+
+        //Whole Program Start
+
+        void programRun()
+        {
+            int startOption = 0;
+            do
+            {
+                programStart();
+                try
+                {
+                    startOption = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter valid entry");
+                    programStart();
+                }
+                if (startOption == 1)
+                {
+                    gamePlay();
+                }
+                else if (startOption == 2)
+                {
+                    showRules();
+                }
+                else if (startOption == 3)
+                {
+                    break;
+                }
+                else
+                {
+                    startOption = 0;
+                }
+            } while (startOption != 3);
+            {
+                Console.WriteLine("Thank You For Playing. Have a Nice Day!....");
+            }
+
+
+        }
+        programRun();
     }
+
 }
+        
